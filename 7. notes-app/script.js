8aed8a524c5143c4786b081be129ffc64d685d09
Anna Ossowska <ossowska.anna1@gmail.com>
2021-06-19 13:31:06 +0200

    Working with localStorage

--- 7. notes-app/script.js
+++ 7. notes-app/script.js
@@ -4,7 +4,9 @@ const addBtn = document.getElementById('add');
 
 const addNewNote = function (text = '') {
   const note = document.createElement('div');
+
   note.classList.add('note');
+
   note.innerHTML = `
   <div class="tool">
     <button class="edit"><i class="fas fa-edit"></i></button>
@@ -15,12 +17,14 @@ const addNewNote = function (text = '') {
   <!-- Show text area when in the edit mode -->
   <textarea class="${text ? 'hidden' : ''}"></textarea>
   `;
+
   document.body.appendChild(note);
 
   const deleteBtn = note.querySelector('.delete');
   const editBtn = note.querySelector('.edit');
   const main = note.querySelector('.main');
   const textArea = note.querySelector('textarea');
+
   textArea.value = text;
   main.innerHTML = marked(text);
   deleteBtn.addEventListener('click', () => {
@@ -34,7 +38,27 @@ const addNewNote = function (text = '') {
 
   textArea.addEventListener('input', (e) => {
     main.innerHTML = marked(e.target.value);
+
+    updateLocalSorage();
   });
 };
 
 addBtn.addEventListener('click', () => addNewNote());
+
+const getFromLocalStorage = function () {
+  const textArr = JSON.parse(localStorage.getItem('notes'));
+
+  textArr.forEach((text) => addNewNote(text));
+};
+
+getFromLocalStorage();
+
+const updateLocalSorage = function () {
+  const notesTextArr = [];
+
+  const notesText = document.querySelectorAll('textarea');
+
+  notesText.forEach((n) => notesTextArr.push(n.value));
+
+  localStorage.setItem('notes', JSON.stringify(notesTextArr));
+};
